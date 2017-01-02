24a6774adca9871dce50456675275cb5ef677916
MauroRha <mauro.rha@free.fr>
2017-01-02 17:38:30 +0100

    Add new files

---
+++ WMaster.Library/Enums/MessageCategory.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Original source code in C++ from :
+ * Copyright 2009, 2010, The Pink Petal Development Team.
+ * The Pink Petal Devloment Team are defined as the game's coders 
+ * who meet on http://pinkpetal.org     // old site: http://pinkpetal .co.cc
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//<!-- -------------------------------------------------------------------------------------------------------------------- -->
+//<file>
+//  <copyright file="MessageCategory.cs" company="The Pink Petal Devloment Team">
+//      Copyright © 2009, 2010 - The Pink Petal Devloment Team.
+//  </copyright>
+//  <author>Graben</author>
+//  <datecreated>2016-12-13</datecreated>
+//  <summary>
+//  </summary>
+//  <remarks>
+//      <para name="Review_2016-12">Gbn - 12/2016 : Create / Convert to C# / Refactoring</para>
+//  </remarks>
+//</file>
+//<!-- -------------------------------------------------------------------------------------------------------------------- -->
+namespace WMaster.Enums
+{
+
+    /// <summary>
+    /// Message category. Tell IHM how to present it
+    /// </summary>
+    public enum MessageCategory
+    {
+        COLOR_BLUE = 0,
+        /// <summary>
+        /// Warning message.
+        /// </summary>
+        COLOR_RED = 1,
+        COLOR_DARKBLUE = 2,
+        COLOR_GREEN = 3,
+        COLOR_YELLOW = 4 // `J` added
+    }
+}
